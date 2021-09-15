27f0863cd7f3cc703216d7772b0c354cf881ecc2
JigneshRana <jimmyrana.tech@gmail.com>
2021-09-15 11:57:35 +0530

    new commit

---
+++ settings.php
@@ -0,0 +1,20 @@
+<?php
+$hostname = gethostname();
+$ENVMOD = "Dev";
+$VENDOR_LOCATION = __DIR__."";
+$INTSEC = __DIR__."/../intsec/";
+
+$pattern = "/ip-/i";
+if( preg_match($pattern, $hostname) ){
+    $ENVMOD = "Pro";
+}
+if($ENVMOD == "Dev"){
+    $VENDOR_LOCATION =  __DIR__."";
+}
+elseif($ENVMOD == "Pro"){
+    $VENDOR_LOCATION = __DIR__."/../";
+}
+
+
+
+?>
